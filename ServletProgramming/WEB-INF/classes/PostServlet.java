72834c3bcf4690e8e8b41a839f88d5d519d188c1
sowmya-padmanabhi <sowmya.p.2710@gmail.com>
2018-07-26 11:22:45 -0700

    Removed comments

--- ServletProgramming/WEB-INF/classes/PostServlet.java
+++ ServletProgramming/WEB-INF/classes/PostServlet.java
@@ -2,25 +2,8 @@ import javax.servlet.http.*;
 import javax.servlet.*;
 import java.io.*;
 import java.util.*;
-// import java.io.FileOutputStream;
-// import java.io.IOException;
-// import java.io.InputStream;
-// import java.io.OutputStream;
-// import java.io.PrintWriter;
-// import javax.servlet.ServletException;
-// import javax.servlet.http.HttpServlet;
-// import javax.servlet.http.HttpServletRequest;
-// import javax.servlet.http.HttpServletResponse;
-// import org.json.JSONObject;
-// import org.json.JSONArray;
 import org.json.*;
 import com.oracle.tools.packager.Log;
-
-// import java.io.FileWriter;
-// import org.json.JSONException;
-// import java.util.HashMap;
-// import java.util.Map;
-//import com.sun.org.apache.xerces.internal.impl.xpath.regex.ParseException;
 
 public class PostServlet extends HttpServlet
 {
